b926a47c8f9e614701005bf3ca7ef712f8b03be0
ptratsae <pharrell.tratsaert@ugent.be>
2025-10-21 13:46:57 +0100

    small fix + reposition

--- build/wall_following/prefix_override/sitecustomize.py
+++ build/wall_following/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/zaka/ros2_ws/src/wall_following/install/wall_following'
+    sys.prefix = sys.exec_prefix = '/home/ptratsae/robotics/assignment_1/wall_following/install/wall_following'
