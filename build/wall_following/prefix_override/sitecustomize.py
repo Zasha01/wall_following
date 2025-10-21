8f8ebeb722c1e5de585b862f2cf49d1119c6b9be
ptratsae <pharrell.tratsaert@ugent.be>
2025-10-21 17:41:56 +0100

    "very prototype working"

--- build/wall_following/prefix_override/sitecustomize.py
+++ build/wall_following/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/mnt/c/Users/Wouts/wall_following/install/wall_following'
+    sys.prefix = sys.exec_prefix = '/home/ptratsae/robotics/assignment_1/wall_following/install/wall_following'
